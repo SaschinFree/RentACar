0d582aa01e555f1ee4d7c12d19034dc338291176
SaschinFree <saschinfree@gmail.com>
2022-06-14 18:17:19 +0200

    added UI for manage bookings

---
+++ src/main/java/za/nmu/wrrv/rent/manageBookingsController.java
@@ -0,0 +1,61 @@
+package za.nmu.wrrv.rent;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+
+public class manageBookingsController extends baseController
+{
+    @FXML
+    protected Label currentTab;
+    @FXML
+    protected TextField searchQuery;
+    @FXML
+    protected TableView bookingTable;
+
+
+    @FXML
+    protected void backToMenu(MouseEvent mouseEvent)
+    {
+        if(mouseEvent.getButton() == MouseButton.PRIMARY)
+            nextScene("clerkMenu");
+    }
+
+    @FXML
+    protected void onSearchClicked(MouseEvent mouseEvent)
+    {
+        if(mouseEvent.getButton() == MouseButton.PRIMARY)
+            nextScene("");
+    }
+
+    @FXML
+    protected void onCreateClicked(MouseEvent mouseEvent)
+    {
+        if(mouseEvent.getButton() == MouseButton.PRIMARY)
+            nextScene("createBooking");
+    }
+
+    @FXML
+    protected void onCancelClicked(MouseEvent mouseEvent)
+    {
+        if(mouseEvent.getButton() == MouseButton.PRIMARY)
+            nextScene("cancelBooking");
+    }
+
+    @FXML
+    protected void onBackClicked(MouseEvent mouseEvent)
+    {
+        if(mouseEvent.getButton() == MouseButton.PRIMARY)
+            nextScene("clerkMenu");
+    }
+
+    private void nextScene(String sceneName)
+    {
+        BorderPane fakeMain = (BorderPane) currentTab.getScene().getWindow().getScene().getRoot();
+        fakeMain.setCenter(newCenter(sceneName));
+    }
+}
